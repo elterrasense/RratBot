41d65c3a0d6e7f168a2b2ef109374c9d546f473a
Terra <elterrasense@gmail.com>
2022-02-06 17:52:33 +0100

    Initial commit for Rrat bot

---
+++ src/main/java/com/rrat/ogey/RratBotApplication.java
@@ -0,0 +1,13 @@
+package com.rrat.ogey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RratBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RratBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rrat/ogey/RratBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrat.ogey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RratBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
