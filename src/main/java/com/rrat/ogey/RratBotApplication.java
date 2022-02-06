080919d72c8e9ef88f0e6bbb20f6bab461941c86
Terra <elterrasense@gmail.com>
2022-02-06 18:30:57 +0100

    Added safe token verification

--- src/main/java/com/rrat/ogey/RratBotApplication.java
+++ src/main/java/com/rrat/ogey/RratBotApplication.java
@@ -1,13 +1,28 @@
 package com.rrat.ogey;
 
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class RratBotApplication {
 
+	@Autowired
+	private Environment env;
+
 	public static void main(String[] args) {
 		SpringApplication.run(RratBotApplication.class, args);
 	}
-
+	@Bean
+	@ConfigurationProperties(value = "discord-api")
+	public DiscordApi discordApi(){
+		String token = env.getProperty("TOKEN");
+		DiscordApi api = new DiscordApiBuilder().setToken(token).setAllNonPrivilegedIntents().login().join();
+		return api;
+	}
 }
