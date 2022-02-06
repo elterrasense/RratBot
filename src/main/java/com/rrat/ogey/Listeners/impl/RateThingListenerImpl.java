08a78d159f2d68c7ed12bb5f75e7712a766f81d7
Terra <elterrasense@gmail.com>
2022-02-06 22:56:08 +0100

    Add a one in a thousand chances of giving an 11/10 score.

--- src/main/java/com/rrat/ogey/Listeners/impl/RateThingListenerImpl.java
+++ src/main/java/com/rrat/ogey/Listeners/impl/RateThingListenerImpl.java
@@ -1,6 +1,4 @@
 package com.rrat.ogey.Listeners.impl;
-
-import com.rrat.ogey.Listeners.RateListener;
 import com.rrat.ogey.Listeners.RateThingListener;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.springframework.stereotype.Component;
@@ -18,11 +16,17 @@ public class RateThingListenerImpl implements RateThingListener {
         if (messageCreateEvent.getMessageContent().startsWith("!rate")) {
             Matcher matcher = pattern.matcher(messageCreateEvent.getMessageContent());
             if (matcher.matches()) {
-                //Rating from 0 to 10 using Math.random
-                int rating = (int) (Math.random()*(10))+ 1;
-                messageCreateEvent.getChannel()
-                        .sendMessage(
-                            "I rate " + matcher.group(1) + " " + rating + "/10");
+                //Small chance of giving 11/10
+                int chance = (int) (Math.random()*(1000) + 1);
+                if (chance < 10) {
+                    messageCreateEvent.getChannel().sendMessage("I rate " + matcher.group(1) + " 11/10");
+                } else {
+                    //Rating from 0 to 10 using Math.random
+                    int rating = (int) (Math.random()*(10))+ 1;
+                    messageCreateEvent.getChannel()
+                            .sendMessage(
+                                    "I rate " + matcher.group(1) + " " + rating + "/10");
+                }
             } else {
                 //Send help syntax message
                 messageCreateEvent.getChannel().sendMessage("Incorrect syntax, are you trying to use `!rate [thing]`?");
