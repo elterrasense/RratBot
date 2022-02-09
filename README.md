a19a967ebb1e35f76564c479250b9b166afcc6e6
Terra <elterrasense@gmail.com>
2022-02-10 00:11:24 +0100

    Small improvements to "!cockrate" and updated README.md

--- README.md
+++ README.md
@@ -10,4 +10,6 @@ Repository for Rrat Bot
 
 !rate [thing] = Rates anything you input on a scale from 0 to 10 (and maybe more)
 
+!cockrate [thing] = Gives the exact cock's length of anything you input
+
 The bot can also react to certain keywords which I won't list as it's funnier to find out by oneself.
--- src/main/java/com/rrat/ogey/listeners/impl/CockRateCommandExecutor.java
+++ src/main/java/com/rrat/ogey/listeners/impl/CockRateCommandExecutor.java
@@ -5,6 +5,7 @@ import org.javacord.api.event.message.MessageCreateEvent;
 import org.springframework.stereotype.Component;
 
 import java.text.DecimalFormat;
+import java.util.Random;
 
 @Component
 public class CockRateCommandExecutor implements CommandExecutor {
@@ -15,10 +16,16 @@ public class CockRateCommandExecutor implements CommandExecutor {
     public void execute(MessageCreateEvent event, String arguments) {
         if (arguments != null && !"".equals(arguments)) {
             //Get a length from 1 cm to 25 cm then convert it to imperial
-            double lengthCm = 1 + (Math.abs(arguments.hashCode()) % 25);
+            double lengthCm = 10 + 10 * (new Random(arguments.hashCode()).nextDouble());
             double lengthInch =  lengthCm * 0.394;
-            event.getChannel().sendMessage(arguments + "'s" + " cock is " + lengthCm + " cm/"
-                    + df.format(lengthInch) + " inches long");
+            if (lengthCm > 0) {
+                event.getChannel().sendMessage(arguments + "'s" + " cock is " + df.format(lengthCm) + " cm/"
+                        + df.format(lengthInch) + " inches long");
+            } else {
+                //Variation if values are negative
+                event.getChannel().sendMessage(arguments + "'s" + " cock is so short it became a vagina! It's" + df.format(lengthCm) + " cm/"
+                        + df.format(lengthInch) + " inches deep");
+            }
         } else {
             event.getChannel().sendMessage("Incorrect syntax, are you trying to use `!cockrate [thing]`?");
         }
