32a44abfd4e601fa7cf9f7e40fdda1fd1347855e
Terra <elterrasense@gmail.com>
2022-02-14 18:18:16 +0100

    Updated font name

--- src/main/java/com/rrat/ogey/listeners/impl/AddCaptionCommandExecutor.java
+++ src/main/java/com/rrat/ogey/listeners/impl/AddCaptionCommandExecutor.java
@@ -32,7 +32,7 @@ public class AddCaptionCommandExecutor implements CommandExecutor {
 
         int imgWidth = bufferedImage.getWidth(), imgHeight = bufferedImage.getHeight(), Lines = 0;
         Graphics2D g = (Graphics2D) bufferedImage.getGraphics();
-        g.setFont(new Font("Arial", Font.PLAIN, (int) (imgHeight * .15)));
+        g.setFont(new Font("Futura XBlk", Font.PLAIN, (int) (imgHeight * .15)));
 
         ArrayList<String> wlines = new ArrayList<>();
         StringBuilder TempLine = new StringBuilder();
@@ -50,7 +50,7 @@ public class AddCaptionCommandExecutor implements CommandExecutor {
         g.dispose();
         BufferedImage sentimage = new BufferedImage(imgWidth, (int) (imgHeight * 1.18 + Lines * g.getFontMetrics().getHeight()), BufferedImage.TYPE_INT_ARGB);
         g = (Graphics2D) sentimage.getGraphics();
-        g.setFont(new Font("Futura Extra Black Condensed", Font.PLAIN, (int) (imgHeight * .15)));
+        g.setFont(new Font("Futura XBlk", Font.PLAIN, (int) (imgHeight * .15)));
         g.drawImage(bufferedImage, 0, (int) (imgHeight * .18 + Lines * g.getFontMetrics().getHeight()), imgWidth, imgHeight, null);
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g.fillRect(0, 0, imgWidth, (int) (imgHeight * .18 + Lines * g.getFontMetrics().getHeight()));
